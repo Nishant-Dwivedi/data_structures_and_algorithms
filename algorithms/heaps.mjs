719cd7cf8fcfb1e7c090ed1ad8deca0d6a30bb00
Nishant <nishant.dwivedi@hotmail.com>
2023-08-27 13:35:35 +0530

    added a priority queue

--- algorithms/heaps.mjs
+++ algorithms/heaps.mjs
@@ -1,20 +1,77 @@
-import heap from "../data structures/heap.mjs";
+import priorityQueue from "../data structures/priorityQueue.mjs";
+// .....................................................................................................................................................................
+// findKthLargest([-1,2,0], 2);                                                                                                //lc:215 medium
+// findClosestElements([1,2,5,5,6,6,7,7,8,9], 7, 7)  // slower(O(n)) than the binary search solution (O(log(n) + k));          //lc:658 medium *
+// topKFrequent([1,1,1,2,2,3], 2)                                                                                              //lc:347 medium
+// .....................................................................................................................................................................
 
+function topKFrequent(nums, k){
+    let frq_map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        frq_map.set(nums[i], frq_map.has(nums[i]) ? frq_map.get(nums[i]) + 1: 1);
+    }
+    let keys = frq_map.keys();
+    let pq = new priorityQueue(keys, comp);
+    function comp (a, b){
+        if(frq_map.get(a) >= frq_map.get(b)){
+            return a
+        }
+        else{
+            return b;
+        }
+    }
+    let res = []
+    for(let i = 0; i < k; i++){
+        res.push(pq.extract_priority_element());
+    }
+    console.log(res);
+    return res
+}
 
+// do binary search instead
+function findClosestElements(arr, k, x){
+    let pq = new priorityQueue(arr, comp);
+    pq.get_priority_element;
+    let res = [];
+    for(let i = 0; i < k; i++){
+        res.push(pq.extract_priority_element())
+        pq.get_priority_element
+    }
+    res = res.sort();
+    console.log(`result: ${res}`);
+    return res;
+    // comparator for the priority queue
+    function comp (a, b){
+        if(Math.abs(a - x) < Math.abs(b - x)){
+            return a;
+        }
+        else if(Math.abs(a - x) == Math.abs(b - x)){
+            return a < b ? a : b;
+        }
+        else return b;
+    }
+}
 
-let newHeap = new heap([1,2,3,4,5,6,7,8,9], "min");
-newHeap.get_min
-newHeap.extract_min()
-newHeap.get_min
-newHeap.insert(0)
-newHeap.get_min
-newHeap.insert(13)
-newHeap.get_min
-newHeap.extract_min()
-newHeap.get_min
-newHeap.extract_min()
-newHeap.get_min
-newHeap.extract_min()
-newHeap.get_min
+function findKthLargest(nums, k){
+    // heap solution: n * log(n);
+    // let res;
+    // // if k's value is smaller than half the size of the array, we build a max heap
+    // if (k <= nums.length/2){
+    //     let max_heap = new heap(nums, "max");
+    //     for(let i = 1; i < k; i++){
+    //         max_heap.extract_max();
+    //     }
+    //     res = max_heap.get_max;
+    // }
+    // else{
+    //     let min_heap = new heap(nums, "min");
+    //     for(let i = 1; i <= nums.length-k; i++){
+    //         min_heap.extract_min();
+    //     }
+    //     res = min_heap.get_min;
+    // }
+    // console.log(res);
+    // return res;
 
-
+    // quick select solution O(n) //todo
+}
---
+++ data structures/priorityQueue.mjs
@@ -0,0 +1,139 @@
+export default class priorityQueue {
+    #heap_arr; 
+    #heap_end_index = 0;
+    #comparator_func;
+    constructor (input, comparator){
+        if(input != undefined && typeof(input) == "object" && input.length != 0 && comparator != undefined){
+             // init heap to be the input array
+             this.#heap_arr = Array.from(input);
+             this.#heap_end_index = this.#heap_arr.length-1;
+             this.#comparator_func = comparator;
+            // build a heap
+            this.#build_heap();
+        }
+        // throw error if invalid input is provided
+        else{
+            return new Error(`Provide a valid argument: arg1: input array of length > 0, arg2: comparator function that takes in two arguments a, b and returns the higher priority element`)
+        }
+        
+    }
+    // helper: builds a heap with highest priority item at the top(max heap)
+    #build_heap(){
+        // heapify the input array
+        for(let i = Math.floor(this.#heap_arr.length/2) - 1; i >= 0; i--){
+            this.#heapify(i);
+        }
+    }
+    // helper: heapifies the subtree rooted at a given index
+    #heapify(index){
+         // return if index points to a leaf node since a leaf node is already a heap
+         if (index > Math.floor((this.#heap_end_index + 1)/2) || index == null){
+            return;
+        }
+    
+        // get the indexes of the left and the right children: 2i+1 and 2i +2 in a 0-indexed array
+         let left = (2 * index) + 1 <= this.#heap_end_index ? (2 * index) + 1 : null;
+         // right child won't necessarily be present
+         let right = 2 * (index) + 2 <= this.#heap_end_index ?  2 * (index) + 2 : null;
+        
+        //  exit early if both child nodes are null
+         if(left == null && right == null){
+            return
+         }
+        //  exit early if current node has higher priority than it's children (both children if both are present, left child if only left is present, or right child if only right child is present), 
+         else if((right == null && left != null && this.#heap_arr[index] == this.#comparator_func(this.#heap_arr[index], this.#heap_arr[left])) || (left == null && right != null && this.#heap_arr[index] == this.#comparator_func(this.#heap_arr[index], this.#heap_arr[right])) || (left != null && right != null && this.#heap_arr[index] == this.#comparator_func(this.#heap_arr[index], this.#heap_arr[left]) && this.#heap_arr[index] == this.#comparator_func(this.#heap_arr[index], this.#heap_arr[right]))){
+            return
+         }
+        //  else there is a violation of the pq property; handle it and recurse on the children nodes.
+         else{
+            // figure out the index of the higher priority child amongst the two of them
+            // let higher_priority_child_index = right != null && left != null ? (this.#heap_arr[right] == this.#comparator_func(this.#heap_arr[right], this.#heap_arr[left]) ? right : left) : (right == null ? left : right); 
+            let higher_priority_child_index;
+            if(right != null && left != null){
+                higher_priority_child_index = this.#heap_arr[right] == this.#comparator_func(this.#heap_arr[right], this.#heap_arr[left]) ? right : left;
+            }
+            else if (right == null){
+                higher_priority_child_index = left;
+            }
+            else{
+                higher_priority_child_index = right;
+            }
+
+            let tmp = this.#heap_arr[index];
+            this.#heap_arr[index] = this.#heap_arr[higher_priority_child_index];
+            this.#heap_arr[higher_priority_child_index] = tmp;
+             // now recursively heapify heaps rooted at children nodes
+            this.#heapify(left);
+            this.#heapify(right);
+        
+         }
+    }
+
+    // gets the highest priority item
+    get get_priority_element(){
+        console.log(`heap: ${this.#heap_arr}, priority element: ${this.#heap_arr[0]}, size: ${this.#heap_end_index+1}`);
+        return this.#heap_arr[0];
+    }
+
+    // returns the current heap size
+    get size(){
+        console.log(`heap: ${this.#heap_arr},size: ${this.#heap_end_index+1}`);
+        return this.#heap_end_index + 1;
+    }
+
+    // removes the highest priority(min) item in a min heap
+    extract_priority_element(){
+        if(this.#heap_arr[0] == null){
+            return
+        }
+        let tmp = this.#heap_arr[0];
+        // copy the left most leaf node to the root node that is to be deleted
+        this.#heap_arr[0] = this.#heap_arr[this.#heap_end_index];
+        // make the left most leaf node null
+        this.#heap_arr[this.#heap_end_index] = null;
+        // decrement the heap array's end pointer
+        this.#heap_end_index--;
+        // heapify the root node
+        this.#heapify(0);
+        return tmp;
+    }
+
+    // inserts a node in the heap
+    insert (value){
+            // if our heap has some empty space left
+            if(this.#heap_arr.length - 1 > this.#heap_end_index){
+                // increment the heap end pointer to reflect the increse in size
+                this.#heap_end_index++;
+                // insert the new element at the left most empty node of the last level(the end of the array)
+                this.#heap_arr[this.#heap_end_index] = value;
+                // undo the heap property violation if there is one after inserting the new node(heapify upwards);
+                let current_index = this.#heap_end_index;
+                let parent_index = Math.floor((current_index-1)/2);
+                // while the higher priority of a parent node isn't restored, keep swapping with child 
+                while(this.#heap_arr[parent_index] != this.#comparator_func(this.#heap_arr[current_index], this.#heap_arr[parent_index])){
+                    let temp = this.#heap_arr[parent_index];
+                    this.#heap_arr[parent_index] = this.#heap_arr[current_index];
+                    this.#heap_arr[current_index] = temp;
+                    current_index = parent_index;
+                    parent_index = Math.floor((current_index-1)/2);
+                }
+            }
+            // else we create a new node and push it to the end of the array
+            else{
+                // push the new node to the array
+                this.#heap_arr.push(value);
+                // increment the heap end pointer to reflect the increse in size
+                this.#heap_end_index++;
+                // undo the heap property violation if there is one after inserting the new node(heapify upwards)
+                let current_index = this.#heap_end_index;
+                let parent_index = Math.floor((current_index-1)/2);
+                while(this.#heap_arr[parent_index] != this.#comparator_func(this.#heap_arr[current_index], this.#heap_arr[parent_index])){
+                    let temp = this.#heap_arr[parent_index];
+                    this.#heap_arr[parent_index] = this.#heap_arr[current_index];
+                    this.#heap_arr[current_index] = temp;
+                    current_index = parent_index;
+                    parent_index = Math.floor((current_index-1)/2);
+                }
+            }
+    }
+}
